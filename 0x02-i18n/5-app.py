af2ab58e198aad46b4ffba40a88c8276bc6d00b6
anithavumba <anithavumba@gmail.com>
2023-08-01 13:52:58 +0200

    appy

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python3
-"""
-Flask app
-"""
-from flask import (
-    Flask,
-    render_template,
-    request,
-    g
-)
-from flask_babel import Babel
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
 
+app = Flask(__name__)
+babel = Babel(app)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -18,59 +11,18 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
-
-class Config(object):
-    """
-    Configuration for Babel
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel(app)
-
-
-def get_user():
-    """
-    Returns a user dictionary or None if ID value can't be found
-    or if 'login_as' URL parameter was not found
-    """
-    id = request.args.get('login_as', None)
-    if id is not None and int(id) in users.keys():
-        return users.get(int(id))
-    return None
-
+from users import get_user
 
 @app.before_request
 def before_request():
-    """
-    Add user to flask.g if user is found
-    """
-    user = get_user()
-    g.user = user
+    user_id = request.args.get("login_as")
+    g.user = get_user(int(user_id)) if user_id else None
 
-
-@babel.localeselector
-def get_locale():
-    """
-    Select and return best language match based on supported languages
-    """
-    loc = request.args.get('locale')
-    if loc in app.config['LANGUAGES']:
-        return loc
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
+@app.route('/')
+def home():
     return render_template('5-index.html')
 
+# ... (code from previous messages)
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run()
